bd504adb398f76dc72b98a27a32da209a976abaf
Shelestenko Nikolay <37602817@qip.ru>
2016-10-19 09:26:11 +0300

    sasha : push1

--- local/templates/pro-69/footer.php
+++ local/templates/pro-69/footer.php
@@ -3,6 +3,25 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 IncludeTemplateLangFile(__FILE__);
 ?>  
 
+<img src='/img/index.jpg' style='width:100%;'>
+
+
+      <?$APPLICATION->IncludeComponent(
+  "bitrix:dw.catalog.index", 
+  ".default", 
+  array(
+    "CACHE_GROUPS" => "Y",
+    "CACHE_TIME" => "36000",
+    "CACHE_TYPE" => "A",
+    "IBLOCK_BINDING" => "element",
+    "IBLOCK_ID" => "5",
+    "LIMIT" => "50",
+    "IBLOCK_TYPE" => "quarters",
+    "COMPONENT_TEMPLATE" => ".default"
+  ),
+  false
+);?>
+
   </body>
 </html>
 
--- local/templates/pro-69/header.php
+++ local/templates/pro-69/header.php
@@ -34,21 +34,3 @@ IncludeTemplateLangFile(__FILE__);
     
 
 
-<img src='/img/index.jpg' style='width:100%;'>
-
-
-      <?$APPLICATION->IncludeComponent(
-  "bitrix:dw.catalog.index", 
-  ".default", 
-  array(
-    "CACHE_GROUPS" => "Y",
-    "CACHE_TIME" => "36000",
-    "CACHE_TYPE" => "A",
-    "IBLOCK_BINDING" => "element",
-    "IBLOCK_ID" => "5",
-    "LIMIT" => "50",
-    "IBLOCK_TYPE" => "quarters",
-    "COMPONENT_TEMPLATE" => ".default"
-  ),
-  false
-);?>
