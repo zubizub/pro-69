60be9306130f882d046be4f767a56693422c0eac
Бубличик <>
2016-10-20 11:43:17 +0300

    slider

---
+++ local/templates/pro-69/distr/www/slider.php
@@ -0,0 +1,231 @@
+﻿<!DOCTYPE html>
+<html lang="ru-RU">
+<head>
+    <title>Заглавная страница</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, maximum-scale=3.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="cleartype" content="on">
+    <!-- IE -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--[if IE]><meta http-equiv="imagetoolbar" content="no"/><meta http-equiv="MSThemeCompatible" content="no"/><![endif]-->
+    <!-- End IE -->
+    <!-- Windows Phone -->
+    <meta name="application-name" content="Заглавная страница">
+    <!-- End Windows Phone -->
+    <title>Заглавная страница — </title>
+    <meta name="author" content="Akado">
+    <!--<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon">-->
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/distr/www/f/min/global.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font1.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font2.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font3.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font4.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/zz.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/vlad.css" media="all">
+    <link rel="start" href="/">
+	
+<style>
+#mfPreviewBar{
+	display: none;
+}
+</style>
+</head>
+<body id="home_page">
+    <div id="wrapper" class="wrapper" style="max-width: 1441px;">
+        <header id="header" class="header">
+            <div class="header-column">
+                <ul class="header-link__list">
+                    <li class="header-link__item header-link__item_address header_link__item_first">
+						Дом на профсоюзной, 69
+                    </li>
+					<li class="header-link__item">+7 495 780-40-40</li>
+                    <li class="header-link__item call"><a class="header-link" href="#">Перезвонить</a></li>
+					<li class="header-link__item write"><a class="header-link" href="#">Написать</a></li>
+					<li class="header-link__item drive"><a class="header-link" href="#">Приехать</a></li>
+					<li class="header-link__item choose" style=""><a class="header-link" href="#">Выбрать<br/>студию</a></li>
+                </ul>
+            </div>
+        </header>
+        <div role="main" id="content" class="content">
+            <div class="logo"><img class="" src="/local/templates/pro-69/distr/www/f/media/logo.jpg" alt="#"></div>
+            <!-- <div class="slider__ content__item"> -->
+            <div class="slider slider--large content__item__">
+                <!-- <div class=""><img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/slide1.png" alt="#"></div> -->
+                <div class=""><img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/quaters/Pro-69_studio_1.jpg" alt="#"></div>
+                <div class=""><img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/quaters/Pro-69_studio_2.jpg" alt="#"></div>
+                <div class=""><img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/quaters/Pro-69_studio_3.jpg" alt="#"></div>
+                <div class=""><img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/quaters/Pro-69_studio_4.jpg" alt="#"></div>
+            </div>
+			<div class="content__item block studios mobile">
+                <div class="slide blue sblock">
+                    <span class="fp fs36">Дом<br/>на Профсоюзной<br/>ждет вас!</span>
+                    <span class="fp fs29">Профсоюзная, 69 -<br/>это светлые, удобные<br/>студии</span>
+                    <div class="rel">
+                        <span class="fg upper studioText">студии</span>
+                        <span class="blockIcon1 fontIcon orange">e</span>
+                    </div>
+
+                </div>
+                
+            </div>
+			<div class="content__item block compact">
+                <div class="slide">
+                    <span class="fp fs53">Компактные<br> и светлые студии</span>
+                    <span class="fg upper studioPrice">от<span>2,9</span></span>
+                    <span class="fg upper fs36">миллионов рублей</span>
+                </div>
+            </div>
+			<div class="content__item block block-double mobile">
+                <div class="content__item five-persent">
+                    <div class=""><img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/slide4_1.jpg" alt="#"></div>
+                    <div class="">
+                        <img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/slide4_2.jpg" alt="#">
+                    </div>
+                </div>
+				<div class="content__item">
+                    <img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/life.jpg" alt="#" style="width:100%;">
+                </div>
+            </div>
+			<div class="content__item block_white ready">
+                <div class="slide">
+                    <span class="fp fs53">Готовность — <br> 80%</span>
+                    <span class="fontIcon">b</span>
+                    <span class="fp fs29 cardColorWhite">Сдача проекта — <br>уже в начале года:</span>
+                    <span class="fg upper studioPrice">2017</span>
+                </div>
+            </div>
+			
+  <div class="content__item block_white metro">
+                <div class="slide">
+                    <span class="fp fs53">7 минут<br>пешком!</span>
+                    <span class="fp fs29">В пешей доступности <br>м.Калужская и м.Беляево</span>
+                    <span class="fg upper">метро</span>
+                </div>
+            </div>
+            <div class="content__item map">
+                <div class="slide">
+                    <img src="/local/templates/pro-69/distr/www/f/media/69_map.jpg" alt="#">
+                </div>
+            </div>
+            <div class="content__item west">
+                <div class="block">
+                    <div>
+                        <span class="fp fs29">Жить в престижном районе.</span>
+                        <p class="fg upper">юго-запад <span>москвы</span></p>
+                    </div>
+                </div>
+                <div class="block_white">
+                    <div>
+                        <span class="fp fs29">Вблизи Дома на Профсоюзной<br>находится: </span>
+                        <div>
+                            <ul class="fp fs20">
+                                <li>Супермаркеты</li>
+                                <li>Торговые центры</li>
+                                <li>Спортивные клубы</li>
+                                <li>Аптеки, школы, дет. сады</li>
+                            </ul>
+                            <ul class="fp fs20">
+                                <li>Институты МФЮА,РГАИС</li>
+                                <li>Отделения банков</li>
+                                <li>Кафе и рестораны</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+			<div class="content-block">
+				<div class="content-block__col_left">
+					
+					<!--./ DYNAMIC LOF TABLE WITH POPUP -->
+
+                      <img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/69_build_2.jpg" alt="#" style="width:100%;">
+
+                    <!--./ END ^-->
+
+				</div>
+				<div class="content-block__col_right">
+
+                    <!--- ИДИ ТЫ НА ХУЙ СО СВОИМ content__item с фиксированной высотой -->
+					<div class="content__item block area autoHeight">
+                         <div class="slide blue sblock">
+                            <span class="fp fs53">Компактные<br> и светлые студии</span>
+                            <span class="fg upper studioText area">
+                                <span class="fl lhCond">от<br/>м2</span>
+                                <span class="fl fs200 mTopMarg10">17</span></span>
+                            <div class="clear clearfix"></div>
+                            <div class="rel">
+                                <a href="javascript:void(0);" class=""><img src="/local/templates/pro-69/distr/www/f/media/plans/plan1.png"/>
+                                <span class="fp fs26 blockSelect">Выбрать</span></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!--     <div class="content__item block_white area2 autoHeight">
+                         <div class="slide blue sblock">
+                            <span class="fp fs40 tBlue">Просторные<br>двухкомнатные<br>студии</span>
+                            <div class="clear clearfix"></div>
+                            <div class="rel area2">
+                                <a href="javascript:void(0);" class="">
+                                    <img src="/local/templates/pro-69/distr/www/f/media/plans/plan2.png"/>
+                                 <span class="fg upper studioText area2">
+                                <span class="fl fs50 lhCond tBlue" >от<br/>м2</span>
+                                <span class="fl fs100 mTopMarg10 tBlue">33</span>
+                            </span>
+                            </div>
+                        </div>
+                     </div> -->
+
+
+				</div>
+			</div>
+        </div>
+    </div>
+    <script type="text/javascript" charset="UTF-8" src="/local/templates/pro-69/distr/www/f/min/libs.min.js"></script>
+    <script type="text/javascript" charset="UTF-8" src="/local/templates/pro-69/distr/www/f/min/global.min.js"></script>
+<script>
+/*$('.slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+
+$('.studios').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false
+});
+
+$('.compact').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false
+});
+$('.five-persent').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false
+});
+
+$('.metro').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false
+});*/
+</script>
+</body>
+</html>
