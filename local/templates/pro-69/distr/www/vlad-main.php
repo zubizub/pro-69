4923a24db9e8c550aa5edb5df954803f361ac64c
zubizub <37602817@qip.ru>
2016-10-21 17:21:37 +0300

    vlad to delete

--- local/templates/pro-69/distr/www/vlad-main.php
+++ local/templates/pro-69/distr/www/vlad-main.php
@@ -20,6 +20,7 @@
     <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font2.css" media="all">
     <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font3.css" media="all">
     <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font4.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/zz.css" media="all">
     <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/vlad.css" media="all">
     <link rel="start" href="/">
 	
@@ -28,6 +29,23 @@
 	display: none;
 }
 </style>
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/distr/www/vlad/slick.css" media="all">
+    <script src="/local/templates/pro-69/distr/www/vlad/jquery-1.12.4.min.js"></script>
+    <script src="/local/templates/pro-69/distr/www/vlad/jquery-1.12.4.min.js"></script>
+    <script src="/local/templates/pro-69/distr/www/vlad/slick.min.js"></script>
+    <script>
+        jQuery(document).ready(function($) {
+            $('.slider-house').slick({
+              dots: true,
+              infinite: true,
+              speed: 300,
+              slidesToShow: 1,
+              adaptiveHeight: true,
+              prevArrow: '<button type="button" class="slick-prev"></button>'
+            });           
+        });
+    </script>
+
 </head>
 <body id="home_page">
     <div id="wrapper" class="wrapper" style="max-width: 1441px;">
@@ -47,7 +65,7 @@
         </header>
         <div role="main" id="content" class="content">
             <div class="logo"><img class="" src="/local/templates/pro-69/distr/www/f/media/logo.jpg" alt="#"></div>
-            <div class="slider content__item">
+            <div class="slider slider--large content__item__">
                 <div class=""><img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/slide1.png" alt="#"></div>
                 <div class=""><img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/slide1.png" alt="#"></div>
             </div>
@@ -70,18 +88,34 @@
                     <span class="fg upper fs36">миллионов рублей</span>
                 </div>
             </div>
-			<div class="content__item block block-double mobile">
+<!-- 			<div class="content__item five-persent">
+                <div class="block_white">
+                    <div class="slide">
+                        <p>
+                            <span>В Октябре скидка на покупку студии!</span>
+                            Прочное готовое здание реновируется и переформатируется с помощью. строительных технологий последнего поколения в уникальный жилой комплекс европейского 
+                        </p>
+                        <p>-5%</p>
+                    </div>
+                </div>
+                <div class="block">
+                    
+                </div>
+            </div> -->
+            <div class="content__item block block-double mobile">
                 <div class="content__item five-persent">
                     <div class=""><img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/slide4_1.jpg" alt="#"></div>
-                    <div class=""><img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/slide4_2.jpg" alt="#"></div>
+                    <div class="">
+                        <img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/slide4_2.jpg" alt="#">
+                    </div>
                 </div>
-				<div class="content__item">
-                    <img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/podpis.jpg" alt="#" style="width:100%;">
+                <div class="content__item">
+                    <img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/life.jpg" alt="#" style="width:100%;">
                 </div>
-            </div>
+            </div>            
 			<div class="content__item block_white ready">
                 <div class="slide">
-                    <span class="fp fs53">Готовность — <br> 80%</span>
+                    <span class="fp fs53">Готовность — <br>80%</span>
                     <span class="fontIcon">b</span>
                     <span class="fp fs29 cardColorWhite">Сдача проекта — <br>уже в начале года:</span>
                     <span class="fg upper studioPrice">2017</span>
@@ -123,6 +157,14 @@
                             </ul>
                         </div>
                     </div>
+                </div>
+            </div>
+
+            <div class="slider-tabs">
+                <div class="slider-house">
+                    <img src="/local/templates/pro-69/distr/www/vlad/slide.jpg" alt="">
+                    <img src="/local/templates/pro-69/distr/www/vlad/slide.jpg" alt="">
+                    <img src="/local/templates/pro-69/distr/www/vlad/slide.jpg" alt="">
                 </div>
             </div>
 			
