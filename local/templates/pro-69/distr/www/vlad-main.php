c8b00147a68cbca1c801eb0c4176404085a6aa27
Vladd-P <vladp@inbox.ru>
2016-10-21 14:40:59 +0300

    q

--- local/templates/pro-69/distr/www/vlad-main.php
+++ local/templates/pro-69/distr/www/vlad-main.php
@@ -20,6 +20,7 @@
     <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font2.css" media="all">
     <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font3.css" media="all">
     <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font4.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/zz.css" media="all">
     <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/vlad.css" media="all">
     <link rel="start" href="/">
 	
