b5f5e41bb2c67429331242bed98061e0a050993f
zubizub <37602817@qip.ru>
2016-10-25 21:57:49 +0300

    module lofts

--- local/templates/pro-69/header.php
+++ local/templates/pro-69/header.php
@@ -6,18 +6,7 @@ IncludeTemplateLangFile(__FILE__);
 <html lang="ru-RU">
 <head>
     <title>Заглавная страница</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, maximum-scale=3.0">
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="cleartype" content="on">
-    <!-- IE -->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!--[if IE]><meta http-equiv="imagetoolbar" content="no"/><meta http-equiv="MSThemeCompatible" content="no"/><![endif]-->
-    <!-- End IE -->
-    <!-- Windows Phone -->
-    <meta name="application-name" content="Заглавная страница">
-    <!-- End Windows Phone -->
-    <title>Заглавная страница — </title>
-    <meta name="author" content="Akado">
+    
     <!--<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon">-->
     <!-- <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/distr/www/f/min/global.css" media="all"> -->
     <link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH;?>/distr/www/f/min/global.css" media="all">
@@ -29,20 +18,14 @@ IncludeTemplateLangFile(__FILE__);
     <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/zz.css" media="all">
     <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/vlad.css" media="all">
     <link rel="start" href="/">
-  <?$APPLICATION->ShowHead()?>
+  
 
 
 
 
-    
-<style>
-#mfPreviewBar{
-    display: none;
-}
-</style>
 </head> 
   <body class="home-page">
-        <?$APPLICATION->ShowPanel();?>
+      
     
 
 
