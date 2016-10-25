8cdb850ef0e3320df4b1bceea7608ffd13032dc7
Welaurs <z500.z500@mail.ru>
2016-10-25 19:57:39 +0300

    card popup

---
+++ local/templates/pro-69/distr/www/card.php
@@ -0,0 +1,101 @@
+<? define(SITE_TEMPLATE_PATH, "/local/templates/pro-69/");?>
+<!DOCTYPE html>
+<html lang="ru-RU">
+
+<head>
+	<title>Заглавная страница</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, maximum-scale=3.0">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="cleartype" content="on">
+	<!-- IE -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!--[if IE]><meta http-equiv="imagetoolbar" content="no"/><meta http-equiv="MSThemeCompatible" content="no"/><![endif]-->
+	<!-- End IE -->
+	<!-- Windows Phone -->
+	<meta name="application-name" content="Заглавная страница">
+	<!-- End Windows Phone -->
+	<title>Заглавная страница — </title>
+	<meta name="author" content="Akado">
+	<!--<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon">-->
+	<!-- <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/distr/www/f/min/global.css" media="all"> -->
+	<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH;?>/distr/www/f/min/global.css" media="all">
+	<!-- <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/distr/www/f/min/global/main.css" media="all"> -->
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font1.css" media="all">
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font2.css" media="all">
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font3.css" media="all">
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font4.css" media="all">
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/zz.css" media="all">
+	<!-- <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/vlad.css" media="all"> -->
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/ser.css" media="all">
+	<link rel="start" href="/">
+	<style>
+		#mfPreviewBar {
+			display: none;
+		}
+	</style>
+</head>
+
+<body id="home_page">
+	<div id="popupBg"></div>
+	<div id="ajaxPopups">
+		<div id="popupClose">X</div>
+		<div style="width:40%;position:relatative;display:inline-block;float:left;">
+			<div class="popupContent">
+			
+				<h2>Компактная и светлая <p>студия</p></h2>
+				<div class="popupInfo">
+					<p>площадь</p>
+					<p class="item">33,50 м²</p>
+					<div class="kostyl"></div>
+					<p>за м²</p>
+					<p class="item">80 000,00 &#8381;</p>
+					<div class="kostyl"></div>
+					<p>общая цена</p>
+					<p class="item">4 200 000,00 &#8381;</p>
+					<div class="kostyl"></div>
+					<table>
+						<thead>
+							<tr>
+								<td><p>этаж</p></td>
+								<td><p>крыло</p></td>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td><p class="item">1</p></td>
+								<td><p class="item">бол.</p></td>
+							</tr>
+						</tbody>
+					</table>
+					<div class="kostyl"></div>
+					<p>статус</p>
+					<p style="color: white;">свободно</p>
+					<button type="button" class="btn-def">Забронировать</button>
+					<div class="popupFooterLeft">
+						<p class="phone">+7 (495) 780-40-40</p>
+						<div class="kostyl"></div>
+						<ul>
+							<li><a href="#">заказать звонок</a></li>
+							<li><a href="#">отправить на почту</a></li>
+							<li><a href="#">скачать</a></li>
+						</ul>
+						<div class="kostyl"></div>
+						<p class="adress">дом на профсоюзной, 69</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div style="width:60%;position:relatative;display:inline-block;float:right;">
+				<div class="plan">
+					
+				</div>
+				<div class="preview">
+				</div>
+			</div>
+	</div>
+	<script type="text/javascript" charset="UTF-8" src="/local/templates/pro-69/distr/www/f/min/libs.min.js"></script>
+	<script type="text/javascript" charset="UTF-8" src="/local/templates/pro-69/distr/www/f/min/global.min.js"></script>
+	<script type="text/javascript" charset="UTF-8" src="/local/templates/pro-69/js/ajax.js"></script>
+</body>
+
+</html>
---
+++ local/templates/pro-69/distr/www/galery.php
@@ -0,0 +1,66 @@
+<? define(SITE_TEMPLATE_PATH, "/pro-69/local/templates/pro-69/");?>
+<!DOCTYPE html>
+<html lang="ru-RU">
+
+<head>
+	<title>Заглавная страница</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, maximum-scale=3.0">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="cleartype" content="on">
+	<!-- IE -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!--[if IE]><meta http-equiv="imagetoolbar" content="no"/><meta http-equiv="MSThemeCompatible" content="no"/><![endif]-->
+	<!-- End IE -->
+	<!-- Windows Phone -->
+	<meta name="application-name" content="Заглавная страница">
+	<!-- End Windows Phone -->
+	<title>Заглавная страница — </title>
+	<meta name="author" content="Akado">
+	<!--<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon">-->
+	<!-- <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/distr/www/f/min/global.css" media="all"> -->
+	<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH;?>/distr/www/f/min/global.css" media="all">
+	<!-- <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/distr/www/f/min/global/main.css" media="all"> -->
+	<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH;?>/local/templates/pro-69/fonts/font1.css" media="all">
+	<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH;?>/local/templates/pro-69/fonts/font2.css" media="all">
+	<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH;?>/local/templates/pro-69/fonts/font3.css" media="all">
+	<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH;?>/local/templates/pro-69/fonts/font4.css" media="all">
+	<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH;?>/local/templates/pro-69/css/zz.css" media="all">
+	<!-- <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/vlad.css" media="all"> -->
+	<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH;?>/local/templates/pro-69/css/ser.css" media="all">
+	<link rel="start" href="/">
+	<style>
+		#mfPreviewBar {
+			display: none;
+		}
+	</style>
+</head>
+
+<body id="home_page">
+	<div class="galery">
+		<div class="galery-img">
+			<div class="slider">
+				<!-- <div class=""><img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/slide1.png" alt="#"></div> -->
+				<div class=""><img class="" src="/local/templates/pro-69/distr/www/f/media/quaters/Pro-69_studio_1.jpg" alt="#"></div>
+				<div class=""><img class="" src="/local/templates/pro-69/distr/www/f/media/quaters/Pro-69_studio_2.jpg" alt="#"></div>
+				<div class=""><img class="" src="/local/templates/pro-69/distr/www/f/media/quaters/Pro-69_studio_3.jpg" alt="#"></div>
+				<div class=""><img class="" src="/local/templates/pro-69/distr/www/f/media/quaters/Pro-69_studio_4.jpg" alt="#"></div>
+			</div>
+		</div>
+		<ul class="horizontal-menu">
+			<li>
+				<button type="button" class="btn-galery">123</button>
+			</li>
+			<li>
+				<button type="button" class="btn-galery">123</button>
+			</li>
+			<li>
+				<button type="button" class="btn-galery">123</button>
+			</li>
+		</ul>
+	</div>
+	<script type="text/javascript" charset="UTF-8" src="/local/templates/pro-69/distr/www/f/min/libs.min.js"></script>
+	<script type="text/javascript" charset="UTF-8" src="/local/templates/pro-69/distr/www/f/min/global.min.js"></script>
+	<script type="text/javascript" charset="UTF-8" src="/local/templates/pro-69/js/ajax.js"></script>
+</body>
+
+</html>
