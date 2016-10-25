e9365910c607ef01a46c2128d071130fa3198b83
zubizub <37602817@qip.ru>
2016-10-25 22:01:10 +0300

    module lofts

--- local/templates/pro-69/distr/www/main_popups.php
+++ local/templates/pro-69/distr/www/main_popups.php
@@ -1,7 +1,39 @@
-﻿<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Studio12");
-?>
+﻿<? require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php"); ?>
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die(); ?>
+
+<!DOCTYPE html>
+<html lang="ru-RU">
+
+<head>
+  <title>Заглавная страница</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, maximum-scale=3.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="cleartype" content="on">
+  <!-- IE -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!--[if IE]><meta http-equiv="imagetoolbar" content="no"/><meta http-equiv="MSThemeCompatible" content="no"/><![endif]-->
+  <!-- End IE -->
+  <!-- Windows Phone -->
+  <meta name="application-name" content="Заглавная страница">
+  <!-- End Windows Phone -->
+  <title>Заглавная страница — </title>
+  <meta name="author" content="Akado">
+  <!--<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon">-->
+  <!-- <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/distr/www/f/min/global.css" media="all"> -->
+  <link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH;?>/distr/www/f/min/global.css" media="all">
+  <!-- <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/distr/www/f/min/global/main.css" media="all"> -->
+  <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font1.css" media="all">
+  <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font2.css" media="all">
+  <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font3.css" media="all">
+  <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font4.css" media="all">
+  <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/zz.css" media="all">
+  <!-- <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/vlad.css" media="all"> -->
+  <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/ser.css" media="all">
+  <link rel="start" href="/">
+
+</head>
+
+<body id="home_page">
 
          <?$APPLICATION->IncludeComponent(
   "bitrix:dw.catalog.index", 
@@ -18,4 +50,5 @@ $APPLICATION->SetTitle("Studio12");
   ),
   false
 );?>
-          <? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+    </body>
+  </html>
