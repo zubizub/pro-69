ea61dcdca537114a632ee4eb0add1a2764275942
zubizub <37602817@qip.ru>
2016-10-25 23:06:58 +0300

    module lofts

---
+++ local/templates/pro-69/distr/www/card_inner.php
@@ -0,0 +1,45 @@
+
+			 <div class="popupContent">
+			
+				<h2>Компактная и светлая <p>студия</p></h2>
+				<div class="popupInfo">
+					<p>площадь</p>
+					<p class="item">33,50 м²</p>
+					<div class="kostyl"></div>
+					<p>за м²</p>
+					<p class="item">80 000,00 &#8381;</p>
+					<div class="kostyl"></div>
+					<p>общая цена</p>
+					<p class="item">4 200 000,00 &#8381;</p>
+					<div class="kostyl"></div>
+					<table>
+						<thead>
+							<tr>
+								<td><p>этаж</p></td>
+								<td><p>крыло</p></td>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td><p class="item">1</p></td>
+								<td><p class="item">бол.</p></td>
+							</tr>
+						</tbody>
+					</table>
+					<div class="kostyl"></div>
+					<p>статус</p>
+					<p style="color: white;">свободно</p>
+					<button type="button" class="btn-def">Забронировать</button>
+					<div class="popupFooterLeft">
+						<p class="phone">+7 (495) 780-40-40</p>
+						<div class="kostyl"></div>
+						<ul>
+							<li><a href="#">заказать звонок</a></li>
+							<li><a href="#">отправить на почту</a></li>
+							<li><a href="#">скачать</a></li>
+						</ul>
+						<div class="kostyl"></div>
+						<p class="adress">дом на профсоюзной, 69</p>
+					</div>
+				</div>
+			</div>
