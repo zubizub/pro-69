4497c391db0a4933f362ff9b4d4861d1deade492
zubizub <37602817@qip.ru>
2016-10-31 16:28:11 +0300

    last index.php main

--- index.php
+++ index.php
@@ -299,34 +299,126 @@ $APPLICATION->SetTitle("Заглавная страница"); ?>
                     
                 </div>
             </div> -->
-            <div class="content__item block block-double mobile">
+               <div class="content__item block block-double mobile">
                 <div class="content__item five-persent">
-                    <!-- <div class=""><img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/slide4_1.jpg" alt="#"></div> -->
-                    <!-- <div class=""><img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/news2.jpg" alt="#"></div> -->
-                    <!-- <div class="">
-                        <img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/slide4_2.jpg" alt="#">
-                    </div> -->
+                    <?$APPLICATION->IncludeComponent(
+ "bitrix:news", 
+ "main_offers", 
+ array(
+  "ADD_ELEMENT_CHAIN" => "Y",
+  "ADD_SECTIONS_CHAIN" => "Y",
+  "AJAX_MODE" => "N",
+  "AJAX_OPTION_ADDITIONAL" => "",
+  "AJAX_OPTION_HISTORY" => "N",
+  "AJAX_OPTION_JUMP" => "N",
+  "AJAX_OPTION_STYLE" => "Y",
+  "BROWSER_TITLE" => "-",
+  "CACHE_FILTER" => "N",
+  "CACHE_GROUPS" => "Y",
+  "CACHE_TIME" => "36000000",
+  "CACHE_TYPE" => "A",
+  "CHECK_DATES" => "Y",
+  "COMPONENT_TEMPLATE" => "main_offers",
+  "DETAIL_ACTIVE_DATE_FORMAT" => "d.m.Y",
+  "DETAIL_DISPLAY_BOTTOM_PAGER" => "Y",
+  "DETAIL_DISPLAY_TOP_PAGER" => "N",
+  "DETAIL_FIELD_CODE" => array(
+   0 => "",
+   1 => "",
+  ),
+  "DETAIL_PAGER_SHOW_ALL" => "Y",
+  "DETAIL_PAGER_TEMPLATE" => "",
+  "DETAIL_PAGER_TITLE" => "Страница",
+  "DETAIL_PROPERTY_CODE" => array(
+   0 => "",
+   1 => "AUTHOR_LINK",
+   2 => "",
+  ),
+  "DETAIL_SET_CANONICAL_URL" => "N",
+  "DISPLAY_BOTTOM_PAGER" => "N",
+  "DISPLAY_DATE" => "Y",
+  "DISPLAY_NAME" => "Y",
+  "DISPLAY_PICTURE" => "Y",
+  "DISPLAY_PREVIEW_TEXT" => "Y",
+  "DISPLAY_TOP_PAGER" => "N",
+  "FILTER_FIELD_CODE" => array(
+   0 => "",
+   1 => "",
+  ),
+  "FILTER_NAME" => "",
+  "FILTER_PROPERTY_CODE" => array(
+   0 => "",
+   1 => "AUTHOR_LINK",
+   2 => "SECTION",
+   3 => "",
+  ),
+  "FORUM_ID" => "1",
+  "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+  "IBLOCK_ID" => "1",
+  "IBLOCK_TYPE" => "news",
+  "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+  "LIST_ACTIVE_DATE_FORMAT" => "d.m.Y",
+  "LIST_FIELD_CODE" => array(
+   0 => "",
+   1 => "",
+  ),
+  "LIST_PROPERTY_CODE" => array(
+   0 => "",
+   1 => "AUTHOR_LINK",
+   2 => "",
+  ),
+  "MESSAGES_PER_PAGE" => "10",
+  "MESSAGE_404" => "",
+  "META_DESCRIPTION" => "-",
+  "META_KEYWORDS" => "-",
+  "NEWS_COUNT" => "10",
+  "PAGER_BASE_LINK_ENABLE" => "N",
+  "PAGER_DESC_NUMBERING" => "N",
+  "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+  "PAGER_SHOW_ALL" => "N",
+  "PAGER_SHOW_ALWAYS" => "N",
+  "PAGER_TEMPLATE" => ".default",
+  "PAGER_TITLE" => "Новости",
+  "PATH_TO_SMILE" => "/bitrix/images/forum/smile/",
+  "PREVIEW_TRUNCATE_LEN" => "",
+  "REVIEW_AJAX_POST" => "Y",
+  "SEF_FOLDER" => "/blog/",
+  "SEF_MODE" => "N",
+  "SET_LAST_MODIFIED" => "N",
+  "SET_STATUS_404" => "Y",
+  "SET_TITLE" => "N",
+  "SHOW_404" => "N",
+  "SHOW_LINK_TO_FORUM" => "Y",
+  "SORT_BY1" => "ACTIVE_FROM",
+  "SORT_BY2" => "SORT",
+  "SORT_ORDER1" => "DESC",
+  "SORT_ORDER2" => "ASC",
+  "URL_TEMPLATES_READ" => "",
+  "USE_CAPTCHA" => "Y",
+  "USE_CATEGORIES" => "N",
+  "USE_FILTER" => "Y",
+  "USE_PERMISSIONS" => "N",
+  "USE_RATING" => "N",
+  "USE_REVIEW" => "N",
+  "USE_RSS" => "N",
+  "USE_SEARCH" => "N",
+  "USE_SHARE" => "N",
+  "VARIABLE_ALIASES" => array(
+   "SECTION_ID" => "SECTION_ID",
+   "ELEMENT_ID" => "ELEMENT_ID",
+  )
+ ),
+ false
+);?>
 
-                      <!--<div class="block studios life">
-                          <div class="slide blue sblock life">
-                              <span class="fp fs22">Мечтаете о собственном жилье?</span>
-                              <span class="fp fs18">Комфортные студии<br/>с пропиской в Москве.</span>
-                              <div class="rel">
-                                  <span class="fg studioText life">+ Прописка</span>
-                                  
-                              </div>
-
-                          </div>
-                   <!--  <img class="metro-place__img" src="/local/templates/pro-69/distr/www/f/media/life.jpg" alt="#" style="width:100%;"> 
-                </div>-->
                 
             </div>
-			<div class="content__item block_dark-blue registration">
-				<div class="fp title">Мечтаете о собственном жилье?</div>
-				<p class="fp text">Комфортные студии<br>
-				c пропиской в Москве.</p>
-				<p class="fg brush"><span>+П</span>рописка</div>
-			</div>	         
+   <div class="content__item block_dark-blue registration">
+    <div class="fp title">Мечтаете о собственном жилье?</div>
+    <p class="fp text">Комфортные студии<br>
+    c пропиской в Москве.</p>
+    <p class="fg brush"><span>+П</span>рописка</div>
+   </div>	         
 			<div class="content__item block_white ready">
                 <div class="slide rel">
                     <span class="fp fs53">Готовность — <br>80%</span>
