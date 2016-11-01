3b10e9b2e3a3aaf95202a5671fc72c2ef0275378
zubizub <37602817@qip.ru>
2016-11-01 15:49:42 +0300

    new js+css

---
+++ local/templates/pro-69/js/scripts.js
@@ -0,0 +1,16 @@
+$(".js-phone-mask").mask("+7 (999) 999-99-99");
+
+$('.form__select').each(function () {
+    var $cont = $(this).closest('.form__select-wrap');
+
+    $(this).select2({
+        placeholder: 'Не выбран',
+        dropdownParent: $cont,
+        minimumResultsForSearch: Infinity
+    });
+});
+
+$('#popupClose2').on('click', function(){
+    $('#ajaxPopupCallBack').fadeOut(200);
+    $('#popupBg').fadeOut(200);
+});
