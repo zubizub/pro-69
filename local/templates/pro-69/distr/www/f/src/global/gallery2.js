6b73f97df594fee661d3eb8d4dbb409dc6222b08
zubizub <37602817@qip.ru>
2016-11-02 15:12:04 +0300

    gallery fix

--- local/templates/pro-69/distr/www/f/src/global/gallery2.js
+++ local/templates/pro-69/distr/www/f/src/global/gallery2.js
@@ -1,33 +1,33 @@
-$(document).ready(function() {
-
-	$('.slider2').slick({
-				    dots: false,
-				    infinite: true,
-				    speed: 500,
-				    fade: true,
-				    cssEase: 'linear'
-				});
-
-    $('.gallery2Switch .switch a').click(function(){
-
-
-    		var group = $(this).attr('data-switch');
-
-    		console.log(group);
-
-    		$('.gallery2Switch .switch').removeClass('active');
-    		$(this).parent().addClass('active');
-
-    		$('.galleryGroup').hide();
-    		$('.group'+group).show();
-    		$('.slider'+group).slick({
-				    dots: false,
-				    infinite: true,
-				    speed: 500,
-				    fade: true,
-				    cssEase: 'linear'
-				});
-
-
-    });
-});
+//$(document).ready(function() {
+//
+//	$('.slider2').slick({
+//				    dots: false,
+//				    infinite: true,
+//				    speed: 500,
+//				    fade: true,
+//				    cssEase: 'linear'
+//				});
+//
+//    $('.gallery2Switch .switch a').click(function(){
+//
+//
+//    		var group = $(this).attr('data-switch');
+//
+//    		console.log(group);
+//
+//    		$('.gallery2Switch .switch').removeClass('active');
+//    		$(this).parent().addClass('active');
+//
+//    		$('.galleryGroup').hide();
+//    		$('.group'+group).show();
+//    		$('.slider'+group).slick({
+//				    dots: false,
+//				    infinite: true,
+//				    speed: 500,
+//				    fade: true,
+//				    cssEase: 'linear'
+//				});
+//
+//
+//    });
+//});
--- local/templates/pro-69/js/scripts.js
+++ local/templates/pro-69/js/scripts.js
@@ -44,3 +44,43 @@ $('#btn-pdf').click(function () {
     });
     doc.save('sample-file.pdf');
 });
+
+$('.slider0').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
+
+$('.slider1').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
+
+$('.slider2').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
+
+$('.gallery2Switch .switch a').click(function(){
+
+
+    var group = $(this).attr('data-switch');
+
+    $('.gallery2Switch .switch').removeClass('active');
+    $(this).parent().addClass('active');
+
+    $('.galleryGroup').hide();
+    $('.group'+group).show();
+
+    $(".slider0").slick('setPosition');
+    $(".slider1").slick('setPosition');
+    $(".slider2").slick('setPosition');
+});
