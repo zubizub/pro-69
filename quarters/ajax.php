62a22bf17d4fb518f0011b4ca2e17d68e6c53e64
zubizub <37602817@qip.ru>
2016-11-02 21:30:57 +0300

    css+js last

--- quarters/ajax.php
+++ quarters/ajax.php
@@ -253,7 +253,7 @@
 				print_r($output);
 				//$output[]
 
-				$subject = "Перезвонить. Профсоюзная, 69: $output[lot] ";
+				$subject = "Перезвонить. PRO-69: Лот $output[lot] ";
 				$headers= "MIME-Version: 1.0\r\n";
 				$headers .= "Content-type: text/html; charset=utf-8\r\n";
 
@@ -280,7 +280,7 @@
 				print_r($output);
 				//$output[]
 
-				$subject = "Перезвонить. Профсоюзная, 69:  $output[lot]";
+				$subject = "Перезвонить. PRO-69: Лот $output[lot]";
 				$headers= "MIME-Version: 1.0\r\n";
 				$headers .= "Content-type: text/html; charset=utf-8\r\n";
 
