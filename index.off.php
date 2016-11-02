0d9e090383c1a81f206f4bdd3e8f35761fdd6d8b
zubizub <37602817@qip.ru>
2016-11-02 12:01:07 +0300

    init commit

---
+++ index.off.php
@@ -0,0 +1,64 @@
+﻿<? require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+$APPLICATION->SetTitle("Заглавная страница"); ?>
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die(); ?>
+
+<!DOCTYPE html>
+
+<!DOCTYPE html>
+<html lang="ru-RU">
+<head>
+    <title>Заглавная страница</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, maximum-scale=3.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="cleartype" content="on">
+    <!-- IE -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--[if IE]><meta http-equiv="imagetoolbar" content="no"/><meta http-equiv="MSThemeCompatible" content="no"/><![endif]-->
+    <!-- End IE -->
+    <!-- Windows Phone -->
+    <meta name="application-name" content="Заглавная страница">
+    <!-- End Windows Phone -->
+    <title>Заглавная страница — </title>
+    <meta name="author" content="Akado">
+    <!--<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon">-->
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/distr/www/f/min/global.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font1.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font2.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font3.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font4.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/zz.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/vlad.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/ser.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/popup.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/js/bx/jquery.bxslider.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/local/templates/pro-69/js/fancy/jquery.fancybox.css" media="all">
+    <link rel="start" href="/">
+        <script type="text/javascript" charset="UTF-8" src="/local/templates/pro-69/js/jquery-3.1.1.min.js"></script>
+    
+<style>
+
+#svg{
+  width 80%;
+  stroke-width: 2;
+}
+
+#svg * {
+  fill: #ffffff;
+  fill-opacity: 0;
+  transition: fill-opacity 1s;
+}
+
+#svg.finished * {
+  fill-opacity: 1;
+}
+
+</style>
+
+    <?$APPLICATION->ShowHead()?>
+</head>
+<body id="home_page">
+  
+Ведутся технические работы
+
+</body>
+</html>
