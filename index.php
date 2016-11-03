fbd726b0d75f92b0919c45650a7055f6a3374be9
zubizub <37602817@qip.ru>
2016-11-03 15:20:05 +0300

    metrics+counters

--- index.php
+++ index.php
@@ -967,5 +967,55 @@ $APPLICATION->SetTitle("Заглавная страница"); ?>
     });
 
 </script>
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-73821457-4', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<script type="text/javascript">
+(function ct_load_script() {
+var ct = document.createElement('script'); ct.type = 'text/javascript';
+ct.src = document.location.protocol+'//cc.calltracking.ru/phone.931ff.3696.async.js?nc='+Math.floor(new Date().getTime()/300000);
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ct, s);
+})();
+</script>
+            <!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter40652559 = new Ya.Metrika({
+                    id:40652559,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/40652559" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 </body>
 </html>
