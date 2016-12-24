289884a7b6318b38b15771398f1d0cf3763cc042
Rinaz Shakirov <rinaz.shakir@gmail.com>
2016-12-24 09:47:55 +0300

    fix mobile

--- 404.php
+++ 404.php
@@ -1,20 +1,27 @@
-<?
-include_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/urlrewrite.php');
-
-CHTTP::SetStatus("404 Not Found");
-@define("ERROR_404","Y");
-
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-
-$APPLICATION->SetTitle("404 Not Found");
-
-$APPLICATION->IncludeComponent("bitrix:main.map", ".default", Array(
-	"LEVEL"	=>	"3",
-	"COL_NUM"	=>	"2",
-	"SHOW_DESCRIPTION"	=>	"Y",
-	"SET_TITLE"	=>	"Y",
-	"CACHE_TIME"	=>	"36000000"
-	)
-);
-
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Страница не найдена</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/select2.min.css" media="all">
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/vlad_sasha.css" media="all">
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/distr/www/f/min/global.css" media="all">
+	<!--<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon">-->
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font1.css" media="all">
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font2.css" media="all">
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font3.css" media="all">
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/fonts/font4.css" media="all">
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/popup.css" media="all">
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/js/bx/jquery.bxslider.css" media="all">
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/js/fancy/jquery.fancybox.css" media="all">
+	<link rel="stylesheet" type="text/css" href="/local/templates/pro-69/css/template_styles.css" media="all">
+</head>
+<body style="overflow: hidden">
+<img class="not-page-logo" src="/local/templates/pro-69/distr/www/f/media/logo.jpg" alt="#">
+<div class="not-page">
+	<div class="not-page__title">404</div>
+	<div class="not-page__desc"><span class="yellow">Перейти на</span> <br><a href="/">Главную страницу</a></div>
+</div>
+</body>
+</html>
