27d2a60f762f2a92afd391a50aed8630c90b7e46
Rinaz Shakirov <rinaz.shakir@gmail.com>
2016-12-25 19:50:16 +0300

    fix mobile

--- local/templates/pro-69/components/bitrix/dw.catalog.index/popup-card/template.php
+++ local/templates/pro-69/components/bitrix/dw.catalog.index/popup-card/template.php
@@ -11,15 +11,10 @@ if (is_array($arResult['ITEMS']) && count($arResult['ITEMS']) > 0):
                       <div class="lots__inner">
                           <select class="lot-sort-mobile">
                               <option selected>Сортировать лоты</option>
-                              <option data-sort="number" data-direction="asc">По номеру лота</option>
-                              <option data-sort="wing" data-direction="asc">Крыло</option>
-                              <option data-sort="type" data-direction="asc">Тип</option>
                               <option data-sort="stage" data-direction="asc">Этаж</option>
                               <option data-sort="area" data-direction="asc">Площадь М2</option>
                               <option data-sort="price" data-direction="asc">Цена за М2</option>
                               <option data-sort="cost" data-direction="asc">Стоимость</option>
-                              <option data-sort="decor" data-direction="asc">Отделка</option>
-                              <option data-sort="status" data-direction="asc">Статус</option>
                           </select>
                         <div class="lot lot--header">
                           <div class="lot__value-0">
